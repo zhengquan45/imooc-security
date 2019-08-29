eeebf4299916d4145e5c35d09997c7e366ee3e9a
zhengquan <zheng_quan45@163.com>
2019-08-29 19:37:24 +0800

    no comment

--- imooc-security-demo/src/main/java/com/imooc/aspect/TimeAspect.java
+++ imooc-security-demo/src/main/java/com/imooc/aspect/TimeAspect.java
@@ -1,5 +1,9 @@
 package com.imooc.aspect;
 
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +13,23 @@ import org.springframework.stereotype.Component;
  */
 @Aspect
 @Component
+@Slf4j
 public class TimeAspect {
 
-//    @Around("execution(* com.imooc.web.controller.UserController.*(..))")
+    @Around("execution(* com.imooc.web.controller.UserController.*(..))")
+    public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
+
+        log.info("TimeAspect start");
+        Object[] args = pjp.getArgs();
+        for (Object arg : args) {
+            log.info("arg is:{}",arg.toString());
+        }
+        long startTime = DateUtil.date().getTime();
+        Object proceed = pjp.proceed();
+        long endTime = DateUtil.date().getTime();
+        log.info("TimeAspect end 耗时:{}",endTime-startTime);
+        return proceed;
+
+    }
 
 }
